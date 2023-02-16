5d1f0b6b926697d8a6d5bcd3b84ac797e3a3422c
Khaled Bendary <khaled.samir.bendary@gmail.com>
2023-02-16 08:10:17 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * Main - prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
