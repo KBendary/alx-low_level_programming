2b50db9b729fed7e059ae07becac06164ee8a505
Khaled Bendary <khaled.samir.bendary@gmail.com>
2023-02-21 15:19:12 -0800

    rest

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,23 @@
 
 /**
   * print_last_digit - Prints the last digit of a number
-  * @h: The number to be treated
+  * @n: The number to be treated
   *
   * Return: Value of the last digit of number
   */
 
-int print_last_digit(int h)
+int print_last_digit(int n)
 {
-	_putchar((h % 10) + '0');
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+
+	_putchar(last + '0');
+
+	return (last);
 }
