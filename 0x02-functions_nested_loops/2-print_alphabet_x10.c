c51f2beb9b8019c91a553d2c23d29ce246ee8322
Khaled Bendary <khaled.samir.bendary@gmail.com>
2023-02-21 11:16:17 -0800

    print alphabit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Alwyas 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	for(int i = 1; i <= 10; i++)
+	{
+		print_alphabet();
+	}
+}
