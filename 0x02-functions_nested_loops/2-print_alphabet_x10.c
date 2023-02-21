65b83b99147d5b0b971fb97c89d5e177751547c6
Khaled Bendary <khaled.samir.bendary@gmail.com>
2023-02-21 11:33:50 -0800

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	for(i  = 1; i <= 10; i++)
+	char ch;
+
+	for (i  = 1; i <= 10; i++)
 	{
-		print_alphabet();
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
 	}
 }
