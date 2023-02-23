86825c6c145a001dc7b88b9fd5b80486271b9c07
Khaled Bendary <khaled.samir.bendary@gmail.com>
2023-02-23 15:45:53 -0800

    isupeper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
   * _isupper - Check if a letter is upper
-  * @x: The number to be checked
+  * @c: The number to be checked
   *
   * Return: 1 for upper letter or 0 for any else
   */
 
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
