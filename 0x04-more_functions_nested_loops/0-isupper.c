6755d0bda49caba6f2339079d647d271ec65e3f4
Khaled Bendary <khaled.samir.bendary@gmail.com>
2023-02-23 15:24:55 -0800

    Print triangle.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-
+/**
+  * _isupper - Check if a letter is upper
+  * @x: The number to be checked
+  *
+  * Return: 1 for upper letter or 0 for any else
+  */
 
 int _isupper(int c)
 {
---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * print_triangle - Prints a triangle of squares according parameter
+  * @size: The size of the squares triangle
+  *
+  * Return: empty
+  */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int c;
+		int s;
+		int p;
+		for(c = 1; c <= size; c++)
+		{
+			for (p = size-1; p = 0; p--)
+			{
+				_putchar(' ');
+			}
+		}
+		{
+			for (s = 1; s <= c; s++)
+			{
+				_putchar('#');
+			}
+		}
+			_putchar('\n');
+	}
+}
