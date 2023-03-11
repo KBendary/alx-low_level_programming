770219ca75ac930b142ceeb9caf8bb5e48785960
Khaled Bendary <khaled.samir.bendary@gmail.com>
2023-03-10 18:26:47 -0800

    new

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,6 +8,7 @@
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
+
 	i = 0;
 	d = 0;
 	n = 0;
@@ -26,23 +27,27 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
+
 			if (d % 2)
+
 				digit = -digit;
+
 			n = n * 10 + digit;
+
 			f = 1;
+
 			if (s[i + 1] < '0' || s[i + 1] > '9')
+
 				break;
+
 			f = 0;
 		}
 		i++;
 	}
 	if (f == 0)
-
 		return (0);
-
 	return (n);
 }
-
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
@@ -56,6 +61,7 @@ int main(int argc, char *argv[])
 	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
+
 		return (1);
 	}
 	num1 = _atoi(argv[1]);
